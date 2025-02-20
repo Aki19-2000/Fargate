b3175f5a79b3ef68395cb0964276c474f1328e5b
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-20 19:17:43 +0530

    Update main.tf

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,1 +1,8 @@
+resource "aws_ecr_repository" "patient_service" {
+  name = var.patient_service_repo_name
+}
+
+resource "aws_ecr_repository" "appointment_service" {
+  name = var.appointment_service_repo_name
+}
 
