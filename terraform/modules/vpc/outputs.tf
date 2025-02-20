92a64588a5d422e3186c5525c37e697b946fb4fa
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-20 22:35:18 +0530

    Update outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -18,3 +18,12 @@ output "private_subnet_2_id" {
   value = aws_subnet.private_subnet_2.id
 }
 
+output "ecs_execution_role_arn" {
+  description = "The ARN of the ECS execution role"
+  value       = aws_iam_role.ecs_execution_role.arn
+}
+
+output "ecs_task_role_arn" {
+  description = "The ARN of the ECS task role"
+  value       = aws_iam_role.ecs_task_role.arn
+}
