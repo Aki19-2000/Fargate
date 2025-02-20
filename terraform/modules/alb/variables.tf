ab7ad364da4ea4bf41bb650a8dd28553030cd2a3
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-20 19:19:02 +0530

    Update variables.tf

--- terraform/modules/alb/variables.tf
+++ terraform/modules/alb/variables.tf
@@ -1,1 +1,20 @@
+variable "alb_name" {
+  description = "The name of the Application Load Balancer"
+  type        = string
+}
+
+variable "lb_security_groups" {
+  description = "The security groups associated with the load balancer"
+  type        = list(string)
+}
+
+variable "lb_subnets" {
+  description = "The subnets where the load balancer will be deployed"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "The VPC ID for the load balancer"
+  type        = string
+}
 
