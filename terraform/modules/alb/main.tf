0ba08a004767fa7496db8d9b6ba65c9c22b83e5e
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-20 19:18:42 +0530

    Update main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,1 +1,26 @@
+resource "aws_lb" "app_lb" {
+  name               = var.alb_name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups   = var.lb_security_groups
+  subnets           = var.lb_subnets
+}
+
+resource "aws_lb_target_group" "patient_tg" {
+  name     = "patient-service-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.patient_tg.arn
+  }
+}
 
