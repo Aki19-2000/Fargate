d994d9ceb9496e3fba36a5ed8bbfb741c96e0a97
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-20 19:38:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 # main.tf - Entry point for Terraform, calls the modules
 
 module "vpc" {
-  source              = "./modules/vpc"
-  vpc_cidr            = var.vpc_cidr
-  vpc_name            = "my-vpc"
+  source               = "./modules/vpc"
+  vpc_cidr             = var.vpc_cidr
+  vpc_name             = "my-vpc"
   public_subnet_1_cidr = var.public_subnet_1_cidr
   private_subnet_1_cidr = var.private_subnet_1_cidr
   public_subnet_2_cidr = var.public_subnet_2_cidr
@@ -13,14 +13,14 @@ module "vpc" {
 }
 
 module "ecs" {
-  source               = "./modules/ecs"
-  ecs_cluster_name     = var.ecs_cluster_name
-  ecs_task_family      = var.ecs_task_family
+  source                = "./modules/ecs"
+  ecs_cluster_name      = var.ecs_cluster_name
+  ecs_task_family       = var.ecs_task_family
   ecs_execution_role_arn = module.vpc.ecs_execution_role_arn
-  ecs_task_role_arn    = module.vpc.ecs_task_role_arn
+  ecs_task_role_arn     = module.vpc.ecs_task_role_arn
   patient_service_image = var.patient_service_image
   subnet_id            = module.vpc.public_subnet_1_id
-  security_group_id    = var.security_group_id
+  security_group_id    = aws_security_group.ecs_sg.id
 }
 
 module "ecr" {
@@ -32,8 +32,47 @@ module "ecr" {
 module "alb" {
   source             = "./modules/alb"
   alb_name           = var.alb_name
-  lb_security_groups = var.lb_security_groups
+  lb_security_groups = [aws_security_group.lb_sg.id]
   lb_subnets         = [module.vpc.public_subnet_1_id, module.vpc.public_subnet_2_id]
   vpc_id             = module.vpc.vpc_id
 }
 
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs_security_group"
+  description = "Allow inbound traffic to ECS services"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "lb_sg" {
+  name        = "lb_security_group"
+  description = "Allow inbound traffic to the ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
