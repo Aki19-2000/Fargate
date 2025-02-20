e0c29a5093c5b724f7e0a35448a4f8b299c3a659
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-20 20:42:04 +0530

    Update server.js

--- app/appointment-service/server.js
+++ app/appointment-service/server.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;  // Default to 3001 or port from the environment
+
+app.get('/', (req, res) => {
+  res.send('Hello from Appointment Service!');  // For appointment-service
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
