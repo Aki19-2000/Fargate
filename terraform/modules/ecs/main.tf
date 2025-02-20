7aebfbddbf6b84de05e91d80d0502962173950cd
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-20 19:16:34 +0530

    Update main.tf

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -1,1 +1,40 @@
+resource "aws_ecs_cluster" "main" {
+  name = var.ecs_cluster_name
+}
+
+resource "aws_ecs_task_definition" "patient_service" {
+  family                   = var.ecs_task_family
+  execution_role_arn       = var.ecs_execution_role_arn
+  task_role_arn            = var.ecs_task_role_arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+
+  container_definitions = jsonencode([{
+    name      = "patient-service"
+    image     = var.patient_service_image
+    cpu       = 256
+    memory    = 512
+    essential = true
+    portMappings = [
+      {
+        containerPort = 3001
+        hostPort      = 3001
+        protocol      = "tcp"
+      }
+    ]
+  }])
+}
+
+resource "aws_ecs_service" "patient_service" {
+  name            = "patient-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.patient_service.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = [var.subnet_id]
+    security_groups = [var.security_group_id]
+    assign_public_ip = true
+  }
+}
 
