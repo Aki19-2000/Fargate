5431325c1a2608606cd380fc0015ea041f16a5c4
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-22 01:22:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,5 +75,5 @@ module "alb" {
   lb_security_groups         = [aws_security_group.lb_sg.id]
   lb_subnets                 = [module.vpc.public_subnet_1_id, module.vpc.public_subnet_2_id]
   vpc_id                     = module.vpc.vpc_id
-  # Removed patient_service_id and appointment_service_id as you are manually attaching them
+  # Removed patient_service_id and appointment_service_id from here
 }
