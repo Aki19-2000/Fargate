cab07e32386f131681348f0e6de8743d9d2f6fe5
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-21 21:30:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,6 +77,7 @@ module "alb" {
   lb_security_groups         = [aws_security_group.lb_sg.id]
   lb_subnets                 = [module.vpc.public_subnet_1_id, module.vpc.public_subnet_2_id]
   vpc_id                     = module.vpc.vpc_id
-  patient_service_id         = module.ecs.patient_service_id  # Pass the ECS patient service ID
-  appointment_service_id     = module.ecs.appointment_service_id  # Pass the ECS appointment service ID
+  patient_service_id         = module.ecs.patient_service_id  # ECS service ID from module.ecs
+  appointment_service_id     = module.ecs.appointment_service_id  # ECS service ID from module.ecs
 }
+
