0b04d7b7ffa2cc085f3268d14bd4f971226ed27f
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-22 01:33:44 +0530

    Update main.tf

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -1,3 +1,63 @@
+# ECS Cluster
+resource "aws_ecs_cluster" "main" {
+  name = var.ecs_cluster_name
+}
+
+# Patient Service ECS Task Definition
+resource "aws_ecs_task_definition" "patient_service" {
+  family                   = var.ecs_task_family
+  execution_role_arn       = var.ecs_execution_role_arn
+  task_role_arn            = var.ecs_task_role_arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+
+  cpu       = "256"
+  memory    = "512"
+
+  container_definitions = jsonencode([{
+    name      = "patient-service"
+    image     = var.patient_service_image
+    cpu       = 256
+    memory    = 512
+    essential = true
+    portMappings = [
+      {
+        containerPort = 3001
+        hostPort      = 3001
+        protocol      = "tcp"
+      }
+    ]
+  }])
+}
+
+# Appointment Service ECS Task Definition
+resource "aws_ecs_task_definition" "appointment_service" {
+  family                   = var.ecs_task_family
+  execution_role_arn       = var.ecs_execution_role_arn
+  task_role_arn            = var.ecs_task_role_arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+
+  cpu       = "256"
+  memory    = "512"
+
+  container_definitions = jsonencode([{
+    name      = "appointment-service"
+    image     = var.appointment_service_image
+    cpu       = 256
+    memory    = 512
+    essential = true
+    portMappings = [
+      {
+        containerPort = 3002
+        hostPort      = 3002
+        protocol      = "tcp"
+      }
+    ]
+  }])
+}
+
+# Patient Service ECS Service
 resource "aws_ecs_service" "patient_service" {
   name            = "patient-service"
   cluster         = aws_ecs_cluster.main.id
@@ -12,6 +72,7 @@ resource "aws_ecs_service" "patient_service" {
   }
 }
 
+# Appointment Service ECS Service
 resource "aws_ecs_service" "appointment_service" {
   name            = "appointment-service"
   cluster         = aws_ecs_cluster.main.id
