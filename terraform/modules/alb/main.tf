9f2f362f6cda6fdf46cbaeb1b54b6de8bb17879b
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-21 21:40:35 +0530

    Update main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -82,13 +82,20 @@ resource "aws_lb_listener_rule" "appointment_service_rule" {
 
 resource "aws_lb_target_group_attachment" "patient_service_attachment" {
   target_group_arn = aws_lb_target_group.patient_tg.arn
-  target_id        = var.patient_service_id  # Passed from main.tf (module.ecs.patient_service.id)
+  target_id        = var.patient_service_id  # ECS service ID for patient service
   port             = 80
+
+  depends_on = [
+    aws_ecs_service.patient_service  # Ensure patient service is created before attaching
+  ]
 }
 
 resource "aws_lb_target_group_attachment" "appointment_service_attachment" {
   target_group_arn = aws_lb_target_group.appointment_tg.arn
-  target_id        = var.appointment_service_id  # Passed from main.tf (module.ecs.appointment_service.id)
+  target_id        = var.appointment_service_id  # ECS service ID for appointment service
   port             = 80
-}
 
+  depends_on = [
+    aws_ecs_service.appointment_service  # Ensure appointment service is created before attaching
+  ]
+}
