44407c6e73cb8e3e26a6eed45827285241b551d6
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-21 15:50:01 +0530

    Update outputs.tf

--- terraform/modules/ecs/outputs.tf
+++ terraform/modules/ecs/outputs.tf
@@ -6,3 +6,12 @@ output "ecs_task_definition_arn" {
   value = aws_ecs_task_definition.patient_service.arn
 }
 
+output "patient_service_id" {
+  value = aws_ecs_service.patient_service.id
+}
+
+output "appointment_service_id" {
+  value = aws_ecs_service.appointment_service.id
+}
+
+
