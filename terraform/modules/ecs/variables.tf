fac5a05bc16a2537e2095e3f5e7212b30044eadc
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-22 01:28:30 +0530

    Update variables.tf

--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -38,13 +38,5 @@ variable "security_group_id" {
   type        = string
 }
 
-variable "patient_tg_arn" {
-  description = "ARN of the Patient Service Target Group"
-  type        = string
-}
 
-variable "appointment_tg_arn" {
-  description = "ARN of the Appointment Service Target Group"
-  type        = string
-}
 
