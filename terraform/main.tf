f7ab0a979e2f939ab9d07eb41de554b60a646a6e
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-21 16:14:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,11 +72,11 @@ module "ecr" {
 
 # ALB module now directly refers to the lb_sg security group created above
 module "alb" {
-  source             = "./modules/alb"
-  alb_name           = var.alb_name
-  lb_security_groups = [aws_security_group.lb_sg.id]
-  lb_subnets         = [module.vpc.public_subnet_1_id, module.vpc.public_subnet_2_id]
-  vpc_id             = module.vpc.vpc_id
-  patient_service_id = module.ecs.patient_service_id  # Pass ECS service ID for patient service
+  source               = "./modules/alb"
+  alb_name             = var.alb_name
+  lb_security_groups   = [aws_security_group.lb_sg.id]
+  lb_subnets           = [module.vpc.public_subnet_1_id, module.vpc.public_subnet_2_id]
+  vpc_id               = module.vpc.vpc_id
+  patient_service_id   = module.ecs.patient_service_id  # Pass ECS service ID for patient service
   appointment_service_id = module.ecs.appointment_service_id  # Pass ECS service ID for appointment service
 }
