00a2ab4ab4478c37884f85e515382e41a0085126
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-22 17:50:13 +0530

    Update main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,15 +1,24 @@
-# Accept ECS service IP addresses as input variables in the ALB module
-variable "patient_service_id" {
-  description = "The ECS patient service IP"
-  type        = string
+# ALB module - Create load balancer and target group attachments
+
+# Patient Service Target Group
+resource "aws_lb_target_group" "patient_tg" {
+  name     = "patient-service-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "ip"  # Update the target type to IP
 }
 
-variable "appointment_service_id" {
-  description = "The ECS appointment service IP"
-  type        = string
+# Appointment Service Target Group
+resource "aws_lb_target_group" "appointment_tg" {
+  name     = "appointment-service-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "ip"  # Update the target type to IP
 }
 
-# Define the ALB
+# ALB Load Balancer
 resource "aws_lb" "app_lb" {
   name               = var.alb_name
   internal           = false
@@ -17,37 +26,6 @@ resource "aws_lb" "app_lb" {
   security_groups    = var.lb_security_groups
   subnets            = var.lb_subnets
 }
-
-# Define Target Groups with IP as the target type
-resource "aws_lb_target_group" "patient_tg" {
-  name        = "patient-service-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"  # Set target_type to IP
-}
-
-resource "aws_lb_target_group" "appointment_tg" {
-  name        = "appointment-service-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"  # Set target_type to IP
-}
-
-# Attach ECS services to the target groups using IPs
-resource "aws_lb_target_group_attachment" "patient_service_attachment" {
-  target_group_arn = aws_lb_target_group.patient_tg.arn
-  target_id        = aws_ecs_task.patient_service_task.network_interface[0].private_ip  # Correct reference for task's private IP
-  port             = 80
-}
-
-resource "aws_lb_target_group_attachment" "appointment_service_attachment" {
-  target_group_arn = aws_lb_target_group.appointment_tg.arn
-  target_id        = aws_ecs_task.appointment_service_task.network_interface[0].private_ip  # Correct reference for task's private IP
-  port             = 80
-}
-
 
 # ALB Listener for HTTP traffic
 resource "aws_lb_listener" "http_listener" {
@@ -76,7 +54,7 @@ resource "aws_lb_listener_rule" "patient_service_rule" {
   condition {
     path_pattern { 
       values = ["/patient"]
-    } 
+    }
   }
 }
 
@@ -93,4 +71,17 @@ resource "aws_lb_listener_rule" "appointment_service_rule" {
       values = ["/appointment"]
     }
   }
+}
+
+# Attach ECS Service IPs to Target Group (Use IP target type)
+resource "aws_lb_target_group_attachment" "patient_service_attachment" {
+  target_group_arn = aws_lb_target_group.patient_tg.arn
+  target_id        = var.patient_service_ip  # Use ECS task public IP
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "appointment_service_attachment" {
+  target_group_arn = aws_lb_target_group.appointment_tg.arn
+  target_id        = var.appointment_service_ip  # Use ECS task public IP
+  port             = 80
 }
